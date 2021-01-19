74425de9beaa1939088cef1882c87629b26173ce
DhakalRupesh <a73dhakal@outlook.com>
2021-01-19 16:15:09 +0545

    search box

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./Country.css";
 
 const Country = () => {
   return (
     <>
-      <p>country section</p>
+      <div className="countryContainer">
+        <form action="submit">
+          <input
+            type="text"
+            id="countrySearch"
+            placeholder="Search country ...."
+          />
+        </form>
+      </div>
     </>
   );
 };
